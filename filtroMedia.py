1ba19a2a5892aec6baed72fdf1d3efad21ddda6b
MarioTurco <mario.turco4@studenti.unina.it>
2021-11-08 16:16:22 +0100

    added thresholind

--- filtroMedia.py
+++ filtroMedia.py
@@ -4,12 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-image_name = 'roma.jpg'
+image_name = 'foto.jpg'
 def filtroMediaOpenCV(img, kernel_size):
     kernel = np.ones((kernel_size, kernel_size), np.float32)/(kernel_size**2)
     dst = cv.filter2D(img, -1, kernel)
     return dst
 
+def thresholding(image, threshold):
+    new_image = image.copy()
+    if len(new_image.shape) == 3:
+        new_image = cv.cvtColor(new_image, cv.COLOR_BGR2GRAY)
+    for i in range(0, image.shape[0]):
+        for j in range(0, image.shape[1]):
+            if(new_image[i][j] > threshold):
+                new_image[i][j] = 255
+            else:
+                new_image[i][j] = 0
+    return new_image
 
 def filtroMediaMio(image, kernel_size):
     start = time.time()
@@ -34,6 +45,16 @@ def filtroMediaMio(image, kernel_size):
     print("Tempo impiegato senza slicing: ", elapsed)
     return new_image
 
+def localEnhancement(image, level, E):
+    new_image = image.copy()
+    if(len(image.shape) == 3): #se l'immagine non è in scala di grigi convertila
+        new_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    for i in range(0, image.shape[0]):
+        for j in range(0, image.shape[1]):
+            if new_image[i][j] <= level:
+                new_image[i][j] = (new_image[i][j]+1)*E
+    return new_image
+
 def filtroMediaSlicing(image, kernel_size):
     start = time.time()
     new_image = image.copy()
@@ -51,13 +72,20 @@ def filtroMediaSlicing(image, kernel_size):
     return new_image
 
 
-kernel_size = 9
+threshold = 50
+enchancement = 3.5
+level = 25
 image = cv.imread(image_name)
 gray_im = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+new_image = localEnhancement(image, level, enchancement)
 
-plt.subplot(221), plt.imshow(gray_im, cmap='gray'), plt.title('Gray')
-plt.subplot(222), plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB)),plt.title('Original') #OpenCv usa BRG invece che RBG, quindi bisogna invertire il colore
-plt.subplot(223), plt.imshow(filtroMediaMio(gray_im, kernel_size), cmap='gray'), plt.title('Filtro Media Senza Slice ' +   str(kernel_size) +"x" + str(kernel_size))
-plt.subplot(224), plt.imshow(filtroMediaSlicing(gray_im, kernel_size), cmap='gray'), plt.title('Filtro Media Slice ' +   str(kernel_size) +"x" + str(kernel_size))
+plt.subplot(121), plt.imshow(gray_im, cmap='gray'), plt.title('Gray')
+# plt.subplot(222), plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB)),plt.title('Original') #OpenCv usa BRG invece che RBG, quindi bisogna invertire il colore
+plt.subplot(122), plt.imshow(new_image, cmap='gray'), plt.title('Threshold (Level:  ' +   str(threshold) +"/256)" )
 plt.show()
+# plt.subplot(221), plt.imshow(gray_im, cmap='gray'), plt.title('Gray')
+# plt.subplot(222), plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB)),plt.title('Original') #OpenCv usa BRG invece che RBG, quindi bisogna invertire il colore
+# plt.subplot(223), plt.imshow(filtroMediaMio(gray_im, kernel_size), cmap='gray'), plt.title('Filtro Media Senza Slice ' +   str(kernel_size) +"x" + str(kernel_size))
+# plt.subplot(224), plt.imshow(filtroMediaSlicing(gray_im, kernel_size), cmap='gray'), plt.title('Filtro Media Slice ' +   str(kernel_size) +"x" + str(kernel_size))
+# plt.show()
 
